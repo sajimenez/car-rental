5cb2a903f77b940130e8e42613935d76a2fa57d8
Sebastian Jimenez <sajimenez5@gmail.com>
2020-04-30 12:07:06 -0400

    Created Catalog App, added car models

--- src/CarRental/urls.py
+++ src/CarRental/urls.py
@@ -15,10 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.contrib.auth import views as auth_views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('login/', auth_views.LoginView.as_view(), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout')
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+    path('', include('catalog.urls')),
 ]
---
+++ src/catalog/models.py
@@ -0,0 +1,42 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class BaseModel(models.Model):
+    is_active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    last_modified = models.DateTimeField(auto_now=True)
+
+    def deactivate(self):
+        self.is_active = False
+        self.save()
+
+
+class CarModel(BaseModel):
+    class CarDoors(models.IntegerChoices):
+        TWO = 2
+        FOUR = 4
+        FIVE = 5
+
+    class CarSize(models.TextChoices):
+        MINICOMPACT = 'minicompact'
+        COMPACT = 'compact'
+        VAN = 'van'
+        PICKUP = 'pickup'
+
+    name = models.CharField(max_length=255)
+    doors = models.IntegerField(choices=CarDoors.choices)
+    is_diesel = models.BooleanField(default=False)
+    size = models.CharField(max_length=50, choices=CarSize.choices)
+
+    def __str__(self):
+        return self.name
+
+
+class Car(BaseModel):
+    model = models.ForeignKey(CarModel, on_delete=models.CASCADE, related_name='cars')
+
+    def __str__(self):
+        return f'{self.id} - {self.model.name}'
